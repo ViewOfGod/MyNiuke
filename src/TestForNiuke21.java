69cbf92763bf50f87e4e39471afc31f57da7c992
Ligh <liguanghui@hzzl.net>
2019-04-30 18:14:45 +0800

    liguanghui，第21，待解决

---
+++ src/TestForNiuke21.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+ */
+public class TestForNiuke21 {
+
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        if (null == root)
+            return new ArrayList<>();
+        return null;
+    }
+
+}
