2914e92a3ebe3628a4a6653423f8edad85f657ce
Ligh <liguanghui@hzzl.net>
2019-04-30 10:16:41 +0800

    liguanghui，第20，待完善

--- src/TestForNiuke20.java
+++ src/TestForNiuke20.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否可能为该栈的弹出顺序。
  * 假设压入栈的所有数字均不相等。
@@ -7,7 +10,58 @@
 
 public class TestForNiuke20 {
 
-    public boolean IsPopOrder(int[] pushA, int[] popA) {
+    public static void main(String[] args) {
+        int[] in = {1, 2, 3, 4, 5};
+        int[] out = {4, 5, 1, 3, 2};
+        boolean resp = IsPopOrder(in, out);
+        System.out.println(resp);
+    }
+
+    public static boolean IsPopOrder(int[] pushA, int[] popA) {
+        if (null == pushA || null == popA)
+            return false;
+        if (pushA.length != popA.length)
+            return false;
+
+        //思路：首先，我要获取其出栈顺序，然后判断这个顺序是否符合就OK
+        Map<Integer, Integer> mapPush = new HashMap<>();
+        for (int i = 1; i <= pushA.length; i++) {//元素数
+            mapPush.put(pushA[i - 1], i);
+        }
+        //出栈顺序
+        int[] outxu = new int[popA.length];
+        for (int i = 0; i < popA.length; i++) {
+            outxu[i] = mapPush.get(popA[i]);
+            System.out.println(outxu[i]);
+        }
+        System.out.println("--------------------------------------");
+        //校验该顺序是否为出栈顺序
+        for (int i = 0; i < popA.length - 1; i++) {
+            int count = 0;
+            for (int m = i; m < popA.length; m++) {
+                if (outxu[m] < outxu[i])
+                    count++;
+            }
+            System.out.println(count);
+            int[] a = new int[count];
+
+            System.out.println("当前值"+i+"======================================");
+            int x = 0;
+            for (int j = i; j < popA.length; j++) {
+                if (outxu[j] < outxu[i])
+                    a[x++] = j;
+            }
+            for (int z = 0; z < count; z++) {
+                System.out.println(a[z]);
+            }
+            System.out.println("===========================================");
+            int k = a.length - 1;
+            while (k > 0) {
+                if (a[k] < a[k - 1])
+                    return false;
+                k--;
+            }
+        }
         return true;
     }
 
